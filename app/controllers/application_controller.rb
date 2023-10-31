49c24700220d3160ba9b8a2b178e5bf208ef8ff9
MasumaJaffery <masumajaffery1@gmail.com>
2023-10-31 17:06:46 +0500

    Sign Up Successfully!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+  end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         attribute :name, :string
+         validates :name, presence: true
 end
